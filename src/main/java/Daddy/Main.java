2dad69508a64c5771011e29151d2733fa3985af4
Java Courses <JavaCourses@tti.lv>
2017-09-12 20:53:28 +0300

    Initial commit

---
+++ src/main/java/Daddy/Main.java
@@ -0,0 +1,15 @@
+package Daddy;
+
+import asg.cliche.Shell;
+import asg.cliche.ShellFactory;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Notebook commands = new Notebook();
+        Shell shell = ShellFactory.createConsoleShell("hello", "HelloMaven", commands);
+
+        shell.commandLoop();
+    }
+}
